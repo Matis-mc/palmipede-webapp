3cd17ff4557cd4fd6d277b2a55867ab20e992142
charrier <matischarrier@gmail.com>
2022-04-09 15:24:47 +0200

    first commit

---
+++ src/main/java/com/polytech/palmipedewebapp/PalmipedeWebappApplication.java
@@ -0,0 +1,13 @@
+package com.polytech.palmipedewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalmipedeWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalmipedeWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polytech/palmipedewebapp/PalmipedeWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polytech.palmipedewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalmipedeWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
