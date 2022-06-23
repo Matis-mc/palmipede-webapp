d910ecd3870b325bfa6ab204b62f50d6cbf8bbdd
charrier <matischarrier@gmail.com>
2022-06-23 19:15:44 +0200

    deploiement en war

--- src/main/java/com/polytech/palmipedewebapp/PalmipedeWebappApplication.java
+++ src/main/java/com/polytech/palmipedewebapp/PalmipedeWebappApplication.java
@@ -2,9 +2,10 @@ package com.polytech.palmipedewebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PalmipedeWebappApplication {
+public class PalmipedeWebappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PalmipedeWebappApplication.class, args);
