8294f0c32152adc2b17de832f411bd290c32729c
charrier <matischarrier@gmail.com>
2022-06-23 21:11:12 +0200

    mode démo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,58 @@
-/*
 CREATE TABLE PALMIPEDE(
-    id_palmipede INT PRIMARY KEY AUTO_INCREMENT,
-    id_espece INTEGER,
-    nom_espece VARCHAR,
-    tag_rfid VARCHAR
+                          id_palmipede INT PRIMARY KEY AUTO_INCREMENT,
+                          id_espece INTEGER,
+                          nom_espece VARCHAR,
+                          tag_rfid VARCHAR
 );
+
+CREATE TABLE PONTE(
+                      id_ponte INT PRIMARY KEY AUTO_INCREMENT,
+                      date DATE,
+                      id_nid INT NOT NULL,
+                      id_palmipede INT NOT NULL
+);
+
+CREATE TABLE ESPECE(
+                       id_espece INT PRIMARY KEY AUTO_INCREMENT,
+                       nom VARCHAR,
+                       poid_minimal_palmipede REAL,
+                       poid_maximal_palmipede REAL,
+                       poid_minimal_oeuf REAL,
+                       poid_maximal_oeuf REAL
+);
+
+CREATE TABLE NID(
+                    id_nid INT PRIMARY KEY AUTO_INCREMENT,
+                    id_antenne INT,
+                    id_balance INT,
+                    id_batiment INT
+);
+
+CREATE TABLE BATIMENT(
+                         id_batiment INT PRIMARY KEY AUTO_INCREMENT,
+                         nom VARCHAR,
+                         id_espece INT
+);
+
+CREATE TABLE BALANCE(
+                        id_balance INT PRIMARY KEY AUTO_INCREMENT,
+
+);
+
+CREATE TABLE ANTENNE_RFID(
+                             id_antenne_rfid INT PRIMARY KEY AUTO_INCREMENT,
+
+);
+
+CREATE TABLE USER(
+                     id_user INT PRIMARY KEY AUTO_INCREMENT,
+                     username VARCHAR,
+                     password VARCHAR,
+                     role VARCHAR,
+                     date_suppression DATE,
+                     is_locked INT
+);
+
 
 INSERT INTO PALMIPEDE(id_palmipede, id_espece, nom_espece, tag_rfid)VALUES( 1, 1, 'oie', '3465');
 INSERT INTO PALMIPEDE(id_palmipede, id_espece, nom_espece, tag_rfid)VALUES( 2, 1, 'oie', '3765');
@@ -13,12 +61,7 @@ INSERT INTO PALMIPEDE(id_palmipede, id_espece, nom_espece, tag_rfid)VALUES( 4, 0
 INSERT INTO PALMIPEDE(id_palmipede, id_espece, nom_espece, tag_rfid)VALUES( 5, 0,'canard', '3245');
 
 
-CREATE TABLE PONTE(
-    id_ponte INT PRIMARY KEY AUTO_INCREMENT,
-    date DATE,
-    id_nid INT NOT NULL,
-    id_palmipede INT NOT NULL
-);
+
 
 INSERT INTO PONTE VALUES ( 0, '2022-05-01', 1, 2 );
 INSERT INTO PONTE VALUES ( 1, '2022-05-12', 2, 3 );
@@ -42,51 +85,29 @@ INSERT INTO PONTE VALUES ( 18, '2022-05-08', 2, 2 );
 INSERT INTO PONTE VALUES ( 19, '2022-05-07', 2, 2 );
 
 
-CREATE TABLE NID(
-    id_nid INT PRIMARY KEY AUTO_INCREMENT,
-    id_antenne INT,
-    id_balance INT,
-    id_batiment INT
-);
+
 
 INSERT INTO NID VALUES ( 0, 0, 0, 1 );
 INSERT INTO NID VALUES ( 1, 1, 3, 0 );
 INSERT INTO NID VALUES ( 2, 3, 1, 0 );
 INSERT INTO NID VALUES ( 3, 2, 2, 1 );
 
-CREATE TABLE ESPECE(
-    id_espece INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR,
-    poid_minimal_palmipede REAL,
-    poid_maximal_palmipede REAL,
-    poid_minimal_oeuf REAL,
-    poid_maximal_oeuf REAL
-);
 
 INSERT INTO ESPECE VALUES(0, 'canard', 1800, 2300, 70, 120);
 INSERT INTO ESPECE VALUES(1, 'oie', 7000, 12000, 125, 150);
 
-CREATE TABLE BATIMENT(
-    id_batiment INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR,
-    id_espece INT
-);
 
 INSERT INTO BATIMENT VALUES(0, 'A', 0);
 INSERT INTO BATIMENT VALUES(1, 'B', 1);
 
-CREATE TABLE BALANCE(
-    id_balance INT PRIMARY KEY AUTO_INCREMENT
-);
+
 
 INSERT INTO BALANCE VALUES(0);
 INSERT INTO BALANCE VALUES(1);
 INSERT INTO BALANCE VALUES(2);
 INSERT INTO BALANCE VALUES(3);
 
-CREATE TABLE ANTENNE_RFID(
-    id_antenne_rfid INT PRIMARY KEY AUTO_INCREMENT
-);
+
 
 INSERT INTO ANTENNE_RFID VALUES(0);
 INSERT INTO ANTENNE_RFID VALUES(1);
