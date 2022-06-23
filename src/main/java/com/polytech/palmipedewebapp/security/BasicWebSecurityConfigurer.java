80961a14e641b4dabcbc07f02470e10e7f58a908
charrier <matischarrier@gmail.com>
2022-06-23 21:03:47 +0200

    cors

--- src/main/java/com/polytech/palmipedewebapp/security/BasicWebSecurityConfigurer.java
+++ src/main/java/com/polytech/palmipedewebapp/security/BasicWebSecurityConfigurer.java
@@ -3,11 +3,7 @@ package com.polytech.palmipedewebapp.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 /*
 @Configuration
@@ -33,7 +29,7 @@ public class BasicWebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST,"/user").permitAll()
                 .antMatchers( "/palmipede", "/ponte").access("hasRole('USER')")
